7db8324a7987012d2193db09b37a5c7868be24bf
Tim James <idoxlr8@gmail.com>
2013-04-03 16:37:49 -0500

    ttrss plugin v0.4
    Small icons

---
+++ init.php
@@ -0,0 +1,31 @@
+<?php
+class article_toolbar_small extends Plugin {
+
+	private $link;
+	private $host;
+
+	function about() {
+		return array(0.4,
+			"Small Toolbar for easy access to feed functions",
+			"idoxlr8", false);
+	}
+
+	function init($host) {
+		$this->link = $host->get_link();
+		$this->host = $host;
+
+		$host->add_hook($host::HOOK_TOOLBAR_BUTTON, $this);
+	}
+
+//	function get_js() {
+//		return file_get_contents(dirname(__FILE__) . "/toolbar.js");
+//	}
+
+	function HOOK_TOOLBAR_BUTTON() {
+		require_once dirname(__FILE__) . "/toolbar_body.php";
+	}
+
+
+
+}
+?>
